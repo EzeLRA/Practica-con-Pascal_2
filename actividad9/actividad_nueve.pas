131373e13a0bcfb14b5218cd6a72406d92fd3716
EzeLRA <ezeleocraft@gmail.com>
2023-04-14 09:10:30 -0300

    actividad9

--- actividad9/actividad_nueve.pas
+++ actividad9/actividad_nueve.pas
@@ -1,12 +1,45 @@
 program actividad_nueve;
 var
-	caracter:char;
-	operacion:integer
+	signo:char;
+	ent,Total:integer;
+	texto,num:String;
 BEGIN
-	readln(caracter);
+
+	ent:=1;
+
+	writeln('Ingrese la operacion:');
+	readln(signo);
 	
-	if((caracter='+')or(caracter='-'))then
-		{IF}
+	if((signo='+')or(signo='-'))then
+		begin
+			writeln('Ingrese un numero:');
+			readln(Total);
+			
+			Str(Total,num);
+			texto:=num;
+			
+			while(ent <> 0)do
+				begin
+					writeln('Ingrese otro numero:');
+					readln(ent);
+					
+					if(signo='+')then
+					Total:=Total+ent;
+					if(signo='-')then
+					Total:=Total-ent;
+					
+					Str(ent,num);
+					texto:=texto+signo+num;
+					
+				end;
+			
+			writeln(' ');
+			writeln('La operacion realizada es la siguiente:');
+			write(Total);	
+			writeln(' = ' + texto);
+		
+		end
 	else
 		writeln('Operacion Invalida');
+	
 END.
